9d03588af1312974c79830cab867a20a587eea37
Vinicius Oliveira <vinicius.shk@gmail.com>
2023-07-22 12:43:05 -0300

    Faz setup inicial da aplicação

---
+++ src/MarketplaceCompleto.Business/Models/Entity.cs
@@ -0,0 +1,13 @@
+﻿namespace Marketplace.Models
+{
+    public abstract class Entity
+    {
+
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+    }
+}
