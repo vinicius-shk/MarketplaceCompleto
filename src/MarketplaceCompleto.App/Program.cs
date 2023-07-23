60b4c9e26b10c942a3c63f0f2692b006e81668d0
Vinicius Oliveira <vinicius.shk@gmail.com>
2023-07-23 12:46:54 -0300

    Mapeia banco de dados

--- src/MarketplaceCompleto.App/Program.cs
+++ src/MarketplaceCompleto.App/Program.cs
@@ -1,4 +1,5 @@
 using MarketplaceCompleto.App.Data;
+using MarketplaceCompleto.Data.Context;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,6 +10,9 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDbContext<MarketplaceDbContext>(options =>
+    options.UseSqlServer(connectionString));
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
---
+++ src/MarketplaceCompleto.Data/Mappings/FornecedorMapping.cs
@@ -0,0 +1,33 @@
+﻿using Marketplace.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MarketplaceCompleto.Data.Mappings
+{
+    public class FornecedorMapping : IEntityTypeConfiguration<Fornecedor>
+    {
+        public void Configure(EntityTypeBuilder<Fornecedor> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Nome)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+
+            builder.Property(x => x.Documento)
+                .IsRequired()
+                .HasColumnType("varchar(14)");
+
+            // 1 : 1 => Fornecedor : Endereco
+            builder.HasOne(f => f.Endereco)
+                .WithOne(e => e.Fornecedor);
+
+            // 1 : N => Fornecedor : Produtos
+            builder.HasMany(f => f.Produtos)
+                .WithOne(p => p.Fornecedor)
+                .HasForeignKey(p => p.FornecedorId);
+
+            builder.ToTable("Fornecedores");
+        }
+    }
+}
